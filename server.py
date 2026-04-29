8f6d19ad34026fdd847bc95d3aa7403d4070c4b9
mystic-systems <forgps123@gmail.com>
2026-04-29 16:22:51 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,9 @@ import numpy as np
 
 app = Flask(__name__)
 CORS(app)
-
+@app.route("/")
+def home():
+    return "Oil AI API is running"
 # -----------------------
 # MARKET DATA
 # -----------------------
